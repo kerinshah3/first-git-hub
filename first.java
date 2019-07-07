14782235026ed0732c379af53be03378a16c4bd1
kerinshah3 <52626899+kerinshah3@users.noreply.github.com>
2019-07-07 09:49:59 -0400

    Update first.java

--- first.java
+++ first.java
@@ -3,9 +3,9 @@ public class main {
   public static void main (String[] args)
   {
   
-    int a=3,b=4,c;
+    int a=3,b=4,c=10;
     
-    c = a + b;
+    c = a + b ;
     Sysytem.out.println(c);
   
   }
